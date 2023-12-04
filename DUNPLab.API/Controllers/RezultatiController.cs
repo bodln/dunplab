cf07ba1a205fb8ccbae78a495d9cdf69e01c73d9
Omer Sadikovic <omer.sadikovic2001@gmail.com>
2023-12-04 21:18:25 +0100

    Add Controller for testing

---
+++ DUNPLab.API/Controllers/RezultatiController.cs
@@ -0,0 +1,28 @@
+﻿using DUNPLab.API.Infrastructure;
+using DUNPLab.API.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DUNPLab.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RezultatiController : ControllerBase
+    {
+        private readonly DunpContext context;
+
+        public RezultatiController(DunpContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public async Task<List<RezultatOdMasine>> GetAll()
+        {
+            return await context.RezultatiOdMasine
+                .Include(rm => rm.SupstanceRezultati)
+                .ToListAsync();
+        }
+    }
+}
